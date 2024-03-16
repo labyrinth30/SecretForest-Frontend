0e61c3971d6af9b16cd9da992e2173f450236148
younha00 <ayeon0@office.skhu.ac.kr>
2024-03-16 21:06:17 +0900

    ✨ [#1]Feat: 메인 페이지 디자인 구현
    구글맵, 가게 정보있는 Footer와 테마 포스터 Carousel Slider로 디자인 완료

--- src/components/HomeHeader.svelte
+++ src/components/HomeHeader.svelte
@@ -1,23 +1,34 @@
 <script>
-	import { router } from 'tinro';
+	import { router } from "tinro";
 
-	const goLogin = () => router.goto('/login');
-    const onLogout = () => router.goto('/');
-
-	
+	const goLogin = () => router.goto("/login");
+	const onLogout = () => router.goto("/");
 </script>
 
-<!-- start header -->
-<header class="main-header">
-  <p class="p-main-title" >비밀의 숲 </p>
-  <nav class="main-nav">
-		<button class="main-menu mr-6" on:click={() => {console.log('1')}}>예약하기</button>    
-    
-  </nav>
 
-		<!--로그인 -->
-		<button class="main-menu mr-6" on:click={() => {console.log('1')}}>로그인</button> 
-	
+<header class="main-header">
+	<p class="p-main-title">비밀의 숲</p>
+	<nav class="main-nav">
+		<button
+		class="main-menu mr-6"
+		on:click={() => {
+			console.log("1");
+		}}>THEME</button
+	>
+		<button
+			class="main-menu mr-6"
+			on:click={() => {
+				console.log("1");
+			}}>RESERVATION</button
+		>
+	</nav>
+
+	<!--로그인 -->
+	<button
+		class="main-menu mr-6"
+		on:click={() => {
+			console.log("1");
+		}}>로그인</button
+	>
 </header>
-<!-- end header -->
 
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,6 +1,9 @@
 <script>
+	import HomeFooter from "../components/HomeFooter.svelte";
+    import HomeTheme from "../components/HomeTheme.svelte";
     import HomeHeader from "../components/HomeHeader.svelte";
 </script>
 
-
 <HomeHeader />
+<HomeTheme />
+<HomeFooter />
