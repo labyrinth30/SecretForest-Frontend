c81815fce7c7f155bdb95b40764e5c936885bd91
younha00 <ayeon0@office.skhu.ac.kr>
2024-03-24 14:28:40 +0900

    ✨ [#18]Feat: 404 Not Found page 구현
    만약 없는 URL로 라우팅하면 script 태그에 의해 Home페이지로 돌아오도록 구현

---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,9 @@
+<div class="not-found-full-box">
+    <h1>Sorry, the page you are looking for does not exist.</h1>    
+
+</div>
+<script>
+    import { router } from 'tinro';
+    const goHome = () => router.goto('/');
+    goHome();
+</script>
